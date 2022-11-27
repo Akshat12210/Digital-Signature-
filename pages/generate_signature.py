d09b4dbcabe79214a75d60dafaeb6c95600b5356
Akshat Modani <akshatmodani@jklu.edu.in>
2022-11-27 16:44:51 +0530

    code for verification and generating signatures

--- pages/generate_signature.py
+++ pages/generate_signature.py
@@ -1,8 +1,32 @@
 #genearte signature page
-#home page
 import streamlit as st
-st.set_page_config(initial_sidebar_state="collapsed")
+import Crypto
+from Crypto import Random
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+from Crypto.Signature import PKCS1_v1_5
+from zipfile import ZipFile
+import os
 
+def rsakeys():  
+     length=1024  
+     privatekey = RSA.generate(length, Random.new().read)  
+     publickey = privatekey.publickey()  
+     return privatekey, publickey
+
+def generate_signature(key, data):
+    print("Generating Signature")
+    h = SHA256.new(data)
+    rsa = RSA.importKey(key)
+    signer = PKCS1_v1_5.new(rsa)
+    signature = signer.sign(h)
+    f = open("D:/Digital-Signature-and-Verification/files/signature.txt", "wb")
+    f.write(signature)
+    f.close()
+    return f
+
+
+st.set_page_config(initial_sidebar_state="collapsed")
 st.text("Cryptography Project")
 
 st.markdown(""" 
@@ -44,8 +68,49 @@ button:hover{
   color: rgb(255, 75, 75);
 }
 </style>
-<a href="/"><button>Home</button></a>
+<a href="/"><button>Home</button></a><br>
 <a href="verify"><button>Verify Signature</button></a>""",unsafe_allow_html=True)
+
+zip=ZipFile("D:/Digital-Signature-and-Verification/files/files.zip",'w')
+
+val=st.file_uploader("Upload data file")
+if(val):
+    f = open("D:/Digital-Signature-and-Verification/files/data.txt", "wb")
+    for i in val:
+        f.write(i)
+    print(val)
+    f.close()
+
+    privatekey,publickey=rsakeys() #generating keys
+
+    privatekey=privatekey.exportKey().decode()
+    publickey=publickey.exportKey().decode()
+
+    print(privatekey)
+    print(publickey)
+
+    f = open('D:/Digital-Signature-and-Verification/files/data.txt', 'rb') # opening a binary file
+    content = f.read()
+    print(content)
+    sign=generate_signature(privatekey,content);
+
+
+    f1 = open("D:/Digital-Signature-and-Verification/files/pub_key.txt", "w")
+    f1.write(publickey)
+    f1.close()
+    f.close()
+
+    os.chdir("D:/Digital-Signature-and-Verification/files/")
+    zip.write("signature.txt")
+    zip.write("pub_key.txt")
+    zip.write("data.txt")
+    zip.close()
+
+    st.text("Please download the zip file containing signature, data file and your public key")
+    with open('D:/Digital-Signature-and-Verification/files/files.zip', 'rb') as f:
+            st.download_button('Download Zip', f, file_name='files.zip')  # Defaults to 'application/octet-stream'
+
+
 
 
 
--- pages/verify.py
+++ pages/verify.py
@@ -1,6 +1,25 @@
 #verification page
-#home page
 import streamlit as st
+import Crypto
+from Crypto import Random
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+from Crypto.Signature import PKCS1_v1_5
+from zipfile import ZipFile
+
+
+def verify_signature(key, data, sig_f):
+    print("Verifying Signature")
+    h = SHA256.new(data)
+    rsa = RSA.importKey(key)
+    signer = PKCS1_v1_5.new(rsa)
+    # with open(sig_f, 'rb') as f: signature = f.read()
+    rsp = "Success" if (signer.verify(h, sig_f)) else " Verification Failure"
+    if(rsp=="Success"):
+        st.success("Success, File is not tampered")
+    else:
+        st.error("Verifcation Failure, File is tampered")
+
 st.set_page_config(initial_sidebar_state="collapsed")
 
 st.text("Cryptography Project")
@@ -48,5 +67,22 @@ button:hover{
 <a href="generate_signature"><button>Generate Signature</button></a>
 """,unsafe_allow_html=True)
 
+st.write("Verify Signature")
+uploaded_zip = st.file_uploader('XML File', type="zip")
+if(uploaded_zip):
+    with ZipFile(uploaded_zip, 'r') as zip_ref:
+        zip_ref.extractall("D:/Digital-Signature-and-Verification/files/uploads")
+    f = open('D:/Digital-Signature-and-Verification/files/uploads/data.txt', 'rb') # opening a binary file
+    content = f.read()
+    print(content)
+    f1 = open('D:/Digital-Signature-and-Verification/files/uploads/signature.txt', 'rb') # opening a binary file
+    sign = f1.read()
+    print(sign)
 
+    f2 = open("D:/Digital-Signature-and-Verification/files/uploads/pub_key.txt", "r")
+    publicKey=f2.read()
+    check=verify_signature(publicKey,content,sign)
+    f1.close()
+    f.close()
+    f2.close()
 
